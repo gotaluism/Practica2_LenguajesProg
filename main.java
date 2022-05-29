a9f84f86758ff429d6c7a9e7f58627a1e70a2ed6
Moisés Arrieta <93001023+Moidah@users.noreply.github.com>
2022-05-29 17:11:28 -0500

    Update main.java

--- main.java
+++ main.java
@@ -11,6 +11,7 @@ public class main{
        Scanner Scan = new Scanner(System.in);
 
 		        System.out.println("¿Que desea hacer?");
+	                System.out.println(" ");
 		        System.out.println("1|Tomar el resultado de una búsqueda (leyendo un archivo plano de la metadata de algún buscador web), \n de un numero (por ejemplo el año de nacimiento de uno de los integrantes del grupo), y \n procesar ese resultado, mostrando 3 hechos relevantes. Usar tipos genéricos y gestionar los posibles valores nulos.");
 		        System.out.println(" ");
 		        System.out.println("2|Tomar el resultado de una búsqueda (leyendo un archivo plano de la metadata de algún buscador web), \n de una palabra (cualquiera que el grupo elija), y procesar ese resultado, mostrando 5 \n hechos relevantes. Usar tipos genéricos y gestionar los posibles valores nulos.");
