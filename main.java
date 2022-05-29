5d43cbb676c59929a3821fff21a0112bf5b93c40
Moisés Arrieta <93001023+Moidah@users.noreply.github.com>
2022-05-29 17:00:00 -0500

    Update main.java

--- main.java
+++ main.java
@@ -8,34 +8,35 @@ import java.util.Scanner;
 public class main{
     public static void main(String[] args){
 
-        System.out.println("¿Que desea hacer?");
-        System.out.println("                   ");
-        System.out.println("1|Tomar el resultado de una búsqueda (leyendo un archivo plano de la metadata de algún buscador web), de un numero (por ejemplo el año de nacimiento de uno de los integrantes del grupo), y procesar ese resultado, mostrando 3 hechos relevantes. Usar tipos genéricos y gestionar los posibles valores nulos.");
-        System.out.println("           ");
-        System.out.println("2|Tomar el resultado de una búsqueda (leyendo un archivo plano de la metadata de algún buscador web), de una palabra (cualquiera que el grupo elija), y procesar ese resultado, mostrando 5 hechos relevantes. Usar tipos genéricos y gestionar los posibles valores nulos.");
-        System.out.println("            ");
-        System.out.println("3|Tomar el resultado de una búsqueda (leyendo un archivo plano de la metadata de algún buscador web), de un texto (más de dos palabras, con espacios), y procesar ese resultado, mostrando 5 hechos relevantes. Usar tipos genéricos y gestionar los posibles valores nulos. Repositorio en GitHub actualizado Sin evidencia/ producto no entregado No se incluyen todos los elementos Estan todos los elementos pero incompletos o incorrectos Todos los elementos ");
+       Scanner Scan = new Scanner(System.in);
 
-        Scanner entrada=new Scanner (System.in);
-        int valor=entrada.nextInt();
-
-        switch (valor){
-            case 1: System.out.println("Hola");
-            break;
-
-            case 2: System.out.println("Hola2");
-            break;
-
-            case 3: System.out.println("Hola3");
-            break;
-
-
-
-
-
-
-        }
-    }
-
-
+		        System.out.println("¿Que desea hacer?");
+		        System.out.println("1|Tomar el resultado de una búsqueda (leyendo un archivo plano de la metadata de algún buscador web), \n de un numero (por ejemplo el año de nacimiento de uno de los integrantes del grupo), y \n procesar ese resultado, mostrando 3 hechos relevantes. Usar tipos genéricos y gestionar los posibles valores nulos.");
+		        System.out.println(" ");
+		        System.out.println("2|Tomar el resultado de una búsqueda (leyendo un archivo plano de la metadata de algún buscador web), \n de una palabra (cualquiera que el grupo elija), y procesar ese resultado, mostrando 5 \n hechos relevantes. Usar tipos genéricos y gestionar los posibles valores nulos.");
+		        System.out.println(" ");
+		        System.out.println("3|Tomar el resultado de una búsqueda (leyendo un archivo plano de la metadata de algún buscador web), \n de un texto (más de dos palabras, con espacios), y procesar ese resultado, mostrando 5 \n hechos relevantes. Usar tipos genéricos y gestionar los posibles valores nulos.");
+       int n = Scan.nextInt();
+       if( n == 1) {
+    	   primero();  
+       }else if(n == 2) {
+    	   segundo();
+       }else {
+    	   tercero();
+       }        
+		        
+		        Scan.close();
+	}
+	public static void primero() {
+		System.out.print("hola");
+		
+ }
+	public static void segundo() {
+		System.out.print("hola2");
+		
+ }
+	public static void tercero() {
+		System.out.print("hola3");
+		
+ }
 }
