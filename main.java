f8799c5511c398dcd8ca221cb1a26aecdbbd8094
gotaluism <76192117+gotaluism@users.noreply.github.com>
2022-05-31 20:16:35 -0500

    Update main.java

--- main.java
+++ main.java
@@ -1,29 +1,30 @@
 import java.util.Scanner;
 public class Main{
     public static void main(String[] args){
-
-       Scanner Scan = new Scanner(System.in);
+        Scanner Scan = new Scanner(System.in);
 
 		  System.out.println("¿Que desea hacer?");
 	                
       System.out.println(" ");
 		        
-      System.out.println("1|Tomar el resultado de una       búsqueda (leyendo un archivo plano de la metadata de algún buscador web), \n de un numero (por ejemplo el año de nacimiento de uno de los integrantes del grupo), y \n procesar ese resultado, mostrando 3 hechos relevantes. Usar tipos genéricos y gestionar los posibles valores nulos.");
+      System.out.println("1|Tomar el resultado de los 5 hechos más relevantes del año -2004-");
 		        
       System.out.println(" ");
 		        
-      System.out.println("2|Tomar el resultado de una búsqueda (leyendo un archivo plano de la metadata de algún buscador web), \n de una palabra (cualquiera que el grupo elija), y procesar ese resultado, mostrando 5 \n hechos relevantes. Usar tipos genéricos y gestionar los posibles valores nulos.");
+      System.out.println("2|Tomar el resultado de los 5 hechos más relevantes de la palabra -corupccion-");
 		        
       System.out.println(" ");
 		        
-      System.out.println("3|Tomar el resultado de una búsqueda (leyendo un archivo plano de la metadata de algún buscador web), \n de un texto (más de dos palabras, con espacios), y procesar ese resultado, mostrando 5 \n hechos relevantes. Usar tipos genéricos y gestionar los posibles valores nulos.");
+      System.out.println("3|Tomar el resultado de los 5 hechos más relevantes de la frase - analisís de pobreza en colombia-");
 
       int n = Scan.nextInt();
-
-       ManipularCSV archivo = new ManipularCSV();
     
-        archivo.leerArchivo("Metadatos1.csv");
-          
+      Funciones funcion = new Funciones();
+
+     
+      funcion.menu(n);
+      
+      
       }
       
 	}
